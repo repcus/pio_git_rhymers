d6fa88050f2c7c5de8fc8b9df0007e51a4b3a16e
Kacper Kornacki <60293257+repcus@users.noreply.github.com>
2020-01-29 02:59:28 +0100

    Add files via upload

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -6,16 +6,13 @@ public class FIFORhymer extends defaultCountingOutRhymer {
 	
 	@Override
 	public int countOut() {
-		while (!callCheck())
-			
-		temp.countIn(super.countOut());
-		
+		while (!callCheck()) {
+			temp.countIn(super.countOut());
+		}
 		int ret = temp.countOut();
-		
-		while (!temp.callCheck())
-			
-		countIn(temp.countOut());
-		
+		while (!temp.callCheck()) {
+			countIn(temp.countOut());
+		}
 		return ret;
 	}
 }
--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -7,28 +7,31 @@ public class defaultCountingOutRhymer {
 	public int total = -1;
 
 	public void countIn(int in) {
-		if (!isFull())
+		if (!isFull()) {
 			NUMBERS[++total] = in;
+		}
 	}
 
-		public boolean callCheck() {
-			return total == -1;
+	public boolean callCheck() {
+		return total == -1;
+	}
+		
+	public boolean isFull() {
+		return total == 11;
+	}
+
+	protected int peekaboo() {
+		if (callCheck()) {
+			return -1;
 		}
-		
-			public boolean isFull() {
-				return total == 11;
-			}
-		
-				protected int peekaboo() {
-					if (callCheck())
-						return -1;
-					return NUMBERS[total];
-				}
-			
-					public int countOut() {
-						if (callCheck())
-							return -1;
-						return NUMBERS[total--];
-					}
+		return NUMBERS[total];
+	}
+
+	public int countOut() {
+		if (callCheck()) {
+			return -1;
+		}
+		return NUMBERS[total--];
+	}
 
 }
